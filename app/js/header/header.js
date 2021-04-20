023bb89eff6fed282bd0f34cb311593dd7986187
Vladislav Leonov <v_leonov1@mail.ru>
2021-04-20 17:23:49 +0300

    fixes 1-13 points

--- app/js/header/header.js
+++ app/js/header/header.js
@@ -2,16 +2,22 @@ export default class Header {
     constructor(root) {
         this.header = $(root);
         const hamburger = root.getElementsByClassName('hamburger')[0];
-        hamburger.addEventListener('click', this.toggleBurger, false);
-
+        
+        this.toggleBurger = this.toggleBurger.bind(this);
         this.handleFixedState();
+
+        hamburger.addEventListener('click', this.toggleBurger, false);
     }
 
-    toggleBurger() {
+    toggleBurger (event) {
         const body = document.getElementsByTagName('body')[0];
         const nav = document.getElementsByClassName('header__nav')[0];
-
-        this.classList.toggle('is-active');
+        
+        if (this.header.hasClass('header--main')) {
+            this.header.toggleClass('header--open-hamburger');
+        }
+        
+        event.currentTarget.classList.toggle('is-active');
         body.classList.toggle('fixed');
         nav.classList.toggle('header__nav--fixed');
     }
