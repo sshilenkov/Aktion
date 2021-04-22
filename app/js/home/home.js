33f7c8dbbdc2aa13a504093a21b96cef47b8ba15
Шиленков Сергей Николаевич <shilenkov@action-media.ru>
2021-04-22 10:07:48 +0300

    Loop video products on main page

--- app/js/home/home.js
+++ app/js/home/home.js
@@ -77,6 +77,7 @@ export default class Home {
                 }
 
                 videoNodes[idx].play();
+                videoNodes[idx].setAttribute('loop', 'loop');
 
                 if (window.innerWidth < 641) {
                     list.scroll({
