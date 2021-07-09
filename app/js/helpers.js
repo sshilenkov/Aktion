8971236155ba903b91987b4e1ae51d41e91de8d1
nikukulkina <nikukulkina@gmail.com>
2021-07-09 17:56:46 +0300

    fix new tabs + first screen video overlay

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -1,21 +1,19 @@
 import {throttle } from 'lodash-es';
 
 export function handleAnimationContainer ($root) {
-  const $container = $root;
 	const $scroll = $('.section__scroll');
 
 	$scroll.lenght && $scroll.addClass('section__scroll--visible');
 
   const handleAnimation = () => {
-    $container.each((index, container) => {
+    $root.each((index, container) => {
       if (container.hasAttribute('data-not-animate')) return;
-
-      if (container.getBoundingClientRect().top <= window.outerHeight - 200) {
+      if (container.getBoundingClientRect().top <= Math.abs(window.outerHeight - 200) || container.getBoundingClientRect().top < window.innerHeight ) {
         container.classList.add('container--animated');
       }
     });
   }
 
   handleAnimation();
-  $(window).on('scroll', throttle(handleAnimation, 150))
+  $(window).on('scroll', throttle(handleAnimation, 150));  
 }
